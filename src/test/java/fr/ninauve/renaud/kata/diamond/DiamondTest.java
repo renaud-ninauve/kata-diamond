3a7e61badf46a8a1a4de02036579020bac962116
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-11-13 16:05:55 +0100

    [refacto] stream

--- src/test/java/fr/ninauve/renaud/kata/diamond/DiamondTest.java
+++ src/test/java/fr/ninauve/renaud/kata/diamond/DiamondTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -48,14 +47,14 @@ public class DiamondTest {
         if (widest == 'A') {
             return List.of("A");
         }
+
         char previousWidest = (char) (widest - 1);
         List<String> previous = printDiamondWithWidestCharLines(previousWidest);
         String widestLine = widestLine(widest, previous);
-        int widestIndex = widest - 'A';
-
-        List<String> previousDuplicatedMiddle = duplicateMiddle(previous);
-        List<String> previousWithMiddleAndSpace = addBeginingAndTrailingSpace(previousDuplicatedMiddle);
-        return insert(previousWithMiddleAndSpace, widestLine, widestIndex);
+        return previous.stream()
+                .map(str -> " " + str + " ")
+                .flatMap(str -> str.contains("" + previousWidest) ? Stream.of(str, widestLine, str) : Stream.of(str))
+                .toList();
     }
 
     private String widestLine(char widest, List<String> previous) {
@@ -64,28 +63,5 @@ public class DiamondTest {
                 .repeat(" ", previous.getFirst().length())
                 .append(widest)
                 .toString();
-    }
-
-    private List<String> addBeginingAndTrailingSpace(List<String> lines) {
-        return lines.stream()
-                .map(line -> " " + line + " ")
-                .toList();
-    }
-
-    private List<String> duplicateMiddle(List<String> lines) {
-        int middle = lines.size() / 2;
-        final List<String> result = new ArrayList<>(lines.subList(0, middle));
-        result.add(lines.get(middle));
-        result.addAll(lines.subList(middle, lines.size()));
-        return result;
-    }
-
-    private List<String> insert(List<String> lines, String newLine, int index) {
-        Stream<String> beforeIndex = lines.subList(0, index).stream();
-        Stream<String> newLineStream = Stream.of(newLine);
-        Stream<String> afterIndex = lines.subList(index, lines.size()).stream();
-
-        Stream<String> untilNewLine = Stream.concat(beforeIndex, newLineStream);
-        return Stream.concat(untilNewLine, afterIndex).toList();
     }
 }
